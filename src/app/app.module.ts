a3390727bb9a8210019e9ca67ca526f9c1e71be6
Mohammed Abdel-Mohsen <mohamed.abdelmohsen@cis.asu.edu.eg>
2017-08-01 14:17:48 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {TranslateModule} from "@ngx-translate/core"
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import {TranslateLoader} from "@ngx-translate/core";
+import{HttpModule} from "@angular/http";
+import {Http} from "@angular/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import{TranslateStaticLoader} from "ng2-translate"
+export function createTranslateLoader(http: Http) {
+  return new TranslateStaticLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +22,14 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
+    TranslateModule.forRoot({loader:{
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    }
+    }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { TranslateService } from '@ngx-translate/core';
+import{availableLanguages,sysOptions} from "./constants"
+import{TranslateModule} from "@ngx-translate/core"
+import{TranslateStaticLoader} from "ng2-translate"
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  languages = availableLanguages;
+  selectedLanguage = sysOptions.systemLanguage;
+  param = { value: 'world' };
+  private translate: TranslateService;
+  constructor(public navCtrl: NavController, translate: TranslateService) {
+    this.translate=translate;
+    this.translate.use('en');
+  // console.log(translate.getDefaultLang());
+  // translate.use('es');
+  // console.log(translate.currentLang);
+  }
+  // applyLanguage() {
+  //   this.translate.use(this.selectedLanguage);
+  // }
+  change(){
+    this.translate.use(this.selectedLanguage);
+    sysOptions.systemLanguage = this.selectedLanguage;
+    //console.log(this.translate.use('es'));
+    //console.log(this.translate);
   }
 
 }
